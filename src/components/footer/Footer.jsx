15d33a09c89154c2055758e5a420e1d8d0c7a2fe
Muskan Garg <muskangarg02270@gmail.com>
2022-09-17 19:52:32 +0530

    remove warnings

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -17,18 +17,18 @@ const Footer = () => {
                     <li><a href='#portfolio'>Projects</a></li>
                     <li><a href='#contact'>Contact</a></li>
                 </ul>
-                <a href="mailto:muskangarg02270@gmail.com" target="_blank" className='footer_logo'>muskangarg02270@gmail.com</a>
+                <a href="mailto:muskangarg02270@gmail.com" className='footer_logo'>muskangarg02270@gmail.com</a>
                 <div className='footer_socials'>
-                    <a href="https://www.linkedin.com/in/muskangarg2409/" target="_blank">
+                    <a href="https://www.linkedin.com/in/muskangarg2409/">
                         <AiFillLinkedin />
                     </a>
-                    <a href="https://github.com/MuskanGarg24" target="_blank">
+                    <a href="https://github.com/MuskanGarg24">
                         <AiFillGithub />
                     </a>
-                    <a href="https://twitter.com/muskan_2409" target="_blank">
+                    <a href="https://twitter.com/muskan_2409">
                         <FaTwitter />
                     </a>
-                    <a href="https://www.instagram.com/muskangarg_2409/" target="_blank">
+                    <a href="https://www.instagram.com/muskangarg_2409/">
                         <FaInstagram /></a>
                 </div>
             </footer>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa';
-import {FaTwitter} from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/muskangarg2409/' target="_blank" className='socialIcons'><BsLinkedin /></a>
-        <a href='https://github.com/MuskanGarg24' target="_blank" className='socialIcons'><FaGithub /></a>
-        <a href='https://twitter.com/muskan_2409' target="_blank" className='socialIcons'><FaTwitter/></a>
-        <a href="https://www.instagram.com/muskangarg_2409/" target="_blank" className='socialIcons'><FaInstagram /></a>
+    // eslint-disable-next-line
+    <div className='header_socials'>  
+      <a href='https://www.linkedin.com/in/muskangarg2409/' className='socialIcons'><BsLinkedin /></a>
+      <a href='https://github.com/MuskanGarg24' className='socialIcons'><FaGithub /></a>
+      <a href='https://twitter.com/muskan_2409' className='socialIcons'><FaTwitter /></a>
+      <a href="https://www.instagram.com/muskangarg_2409/" className='socialIcons'><FaInstagram /></a>
     </div>
   )
 }
