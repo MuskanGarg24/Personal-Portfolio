00d80aabfd8acf5f2b3633dd2b14ae243f97bb16
Muskan Garg <muskangarg02270@gmail.com>
2022-09-17 22:18:42 +0530

    nav links

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ import Experience from './components/experience/Experience';
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from "./components/contact/Contact";
 import Footer from './components/footer/Footer';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
-      <About/>
-      <Experience/>
+      <About />
+      <Experience />
       <Portfolio />
       <Contact />
       <Footer />
