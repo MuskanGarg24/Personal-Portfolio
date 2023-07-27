15a8c81ed27f1dc3f6d92320ee4aa675538e5764
muskan garg <muskangarg02270@gmail.com>
2023-07-27 15:19:06 +0530

    update resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./About.css";
-import ME from '../../assets/me.jpeg'
-import HeaderSocials from '../header/HeaderSocials';
-
+import ME from "../../assets/me.jpeg";
+import HeaderSocials from "../header/HeaderSocials";
 
 const About = () => {
   return (
     <>
-      <section id='about'>
+      <section id="about">
         <h5>Get To Know</h5>
         <h2>About Me</h2>
 
-        <div className='container about_container'>
-          <div className='about_me'>
-            <div className='about_me-image'>
-              <img src={ME} alt='aboutImage'></img>
+        <div className="container about_container">
+          <div className="about_me">
+            <div className="about_me-image">
+              <img src={ME} alt="aboutImage"></img>
             </div>
           </div>
-          <div className='about_content'>
+          <div className="about_content">
             <p>
               Hey there!
-              <br /><br />
-              My name is Muskan Garg, and I am a driven second-year B.Tech. in Information Technology student at MAIT Delhi. I am passionate about software development and have worked as a full-stack developer intern to gain hands-on experience in the field. 
-              <br /><br />
-              I am eager to apply my abilities and further develop my professional experience through various opportunities. My professional attitude is defined by my strong teamwork ethic and a constant desire to learn and grow.
+              <br />
+              <br />
+              My name is Muskan Garg, and I am a driven third-year B.Tech. in
+              Information Technology student at MAIT Delhi. I am passionate
+              about software development and have worked as a full-stack
+              developer intern to gain hands-on experience in the field.
+              <br />
+              <br />I am eager to apply my abilities and further develop my
+              professional experience through various opportunities. My
+              professional attitude is defined by my strong teamwork ethic and a
+              constant desire to learn and grow.
             </p>
             <HeaderSocials />
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,89 +1,158 @@
 // eslint-disable-next-line
-import React from 'react'
-import './Portfolio.css'
-import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.png'
-import IMG3 from '../../assets/portfolio3.png'
-import IMG4 from '../../assets/portfolio4.png'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.png'
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.png";
+import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
+import IMG4 from "../../assets/portfolio4.png";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.png";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
 
-      <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG5} alt="projectImage"></img>
           </div>
           <h3>KwikFood</h3>
-          <p className='paragraph text-light'> Developed a web-based online canteen platform for universities to offer a seamless food ordering experience to users</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/KwikFood-Online-Canteen' className='btn'>Github</a>
-            <a href='https://youtu.be/gg5Y4RZiO-8' className='btn btn-primary'>Video Demo</a>
+          <p className="paragraph text-light">
+            {" "}
+            Developed a web-based online canteen platform for universities to
+            offer a seamless food ordering experience to users
+          </p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/MuskanGarg24/KwikFood-Online-Canteen"
+              className="btn"
+            >
+              Github
+            </a>
+            <a href="https://youtu.be/gg5Y4RZiO-8" className="btn btn-primary">
+              Video Demo
+            </a>
           </div>
         </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG6} alt="projectImage"></img>
           </div>
           <h3>SocioAid</h3>
-          <p className='paragraph text-light'>A web-based platform to help users overcome social anxiety and build confidence</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/SocioAid' className='btn'>Github</a>
-            <a href='https://www.youtube.com/watch?v=xJjl3OfhHtY' className='btn btn-primary'>Video Demo</a>
+          <p className="paragraph text-light">
+            A web-based platform to help users overcome social anxiety and build
+            confidence
+          </p>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com/MuskanGarg24/SocioAid" className="btn">
+              Github
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=xJjl3OfhHtY"
+              className="btn btn-primary"
+            >
+              Video Demo
+            </a>
           </div>
         </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG4} alt="projectImage"></img>
           </div>
           <h3>Landing Page</h3>
-          <p className='paragraph text-light'>The user interface and visual design for a landing page.</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/Landing-Page' className='btn'>Github</a>
-            <a href='https://testing-landing-page.netlify.app/' className='btn btn-primary'>Live Demo</a>
+          <p className="paragraph text-light">
+            The user interface and visual design for a landing page.
+          </p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/MuskanGarg24/Landing-Page"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://testing-landing-page.netlify.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG2} alt="projectImage"></img>
           </div>
           <h3>Crypto Tracker</h3>
-          <p className='paragraph text-light'>A web app that allow users to look at the different cryptocurrencies being traded on the market, as well as monitor their current prices.</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/Crypto-Tracker' className='btn'>Github</a>
-            <a href='https://track-crypto-status.netlify.app/' className='btn btn-primary'>Live Demo</a>
+          <p className="paragraph text-light">
+            A web app that allow users to look at the different cryptocurrencies
+            being traded on the market, as well as monitor their current prices.
+          </p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/MuskanGarg24/Crypto-Tracker"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://track-crypto-status.netlify.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG1} alt="projectImage"></img>
           </div>
           <h3>News Hub</h3>
-          <p className='paragraph text-light'>A short news web app that aggregates the latest news from various sources.</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/News-Hub' className='btn'>Github</a>
-            <a href='https://newzzzhub.netlify.app/' className='btn btn-primary'>Live Demo</a>
+          <p className="paragraph text-light">
+            A short news web app that aggregates the latest news from various
+            sources.
+          </p>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com/MuskanGarg24/News-Hub" className="btn">
+              Github
+            </a>
+            <a
+              href="https://newzzzhub.netlify.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={IMG3} alt="projectImage"></img>
           </div>
           <h3>Time To Geek</h3>
-          <p className='paragraph text-light'>A website where people can post their tech articles/blogs and share with the relevant audience.</p>
-          <div className='portfolio_item-cta'>
-            <a href='https://github.com/MuskanGarg24/Time-To-Geek.git' className='btn'>Github</a>
-            {/* <a href='' className='btn btn-primary'>Live Demo</a> */}
+          <p className="paragraph text-light">
+            A website where people can post their tech articles/blogs and share
+            with the relevant audience.
+          </p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/MuskanGarg24/Time-To-Geek.git"
+              className="btn"
+            >
+              Github
+            </a>
+            <a
+              href="https://timetogeek.netlify.app/"
+              className="btn btn-primary"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
